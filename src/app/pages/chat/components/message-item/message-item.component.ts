499622f7ef6899ee8c1548b8922fb67f8c2bd3ba
Blancojulian <julianblanco963@hotmail.com>
2024-05-10 18:23:35 -0300

    sprint 4

--- src/app/pages/chat/components/message-item/message-item.component.ts
+++ src/app/pages/chat/components/message-item/message-item.component.ts
@@ -17,7 +17,7 @@ export class MessageItemComponent {
     const f = new Date();
     const dif = fechaActual.getTime() - this.fecha.getTime();
     TiempoEnMilisegundos.Dia;
-    if (dif > TiempoEnMilisegundos.Dia) {
+    if (dif > TiempoEnMilisegundos.Dia || (dif <= TiempoEnMilisegundos.Dia && fechaActual.getDay() !== this.fecha.getDay())) {
       return this.fecha.toLocaleDateString('es-ES', {hour: '2-digit', minute:'2-digit'});
       
     } else if (dif > TiempoEnMilisegundos.Hora) {//si es menor a un dia y mayor a una hora mostrar solamente la hora
